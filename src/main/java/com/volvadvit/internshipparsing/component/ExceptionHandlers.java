c6dcf97a00dde7e9e76126b076b6630be765e138
Vadim Volkov <volvadvit@gmail.com>
2021-10-12 11:59:50 +0300

    - Removed unused imports

--- src/main/java/com/volvadvit/internshipparsing/component/ExceptionHandlers.java
+++ src/main/java/com/volvadvit/internshipparsing/component/ExceptionHandlers.java
@@ -2,14 +2,12 @@ package com.volvadvit.internshipparsing.component;
 
 import com.volvadvit.internshipparsing.constant.ResponseMapper;
 import com.volvadvit.internshipparsing.exception.HtmlParseException;
-import com.volvadvit.internshipparsing.utils.LogUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.net.MalformedURLException;
 import java.sql.Timestamp;
 
 /**
--- src/main/java/com/volvadvit/internshipparsing/model/WordToCount.java
+++ src/main/java/com/volvadvit/internshipparsing/model/WordToCount.java
@@ -1,6 +1,5 @@
 package com.volvadvit.internshipparsing.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
